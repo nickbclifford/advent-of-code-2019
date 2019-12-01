b7a8dd8fa425761a61f4d27e7f6998fb3545dc59
Nick Clifford <nick@nickclifford.me>
2019-11-30 23:15:33 -0600

    day 1 solution

--- src/day1.kt
+++ src/day1.kt
@@ -1,3 +1,21 @@
-fun main() {
+import java.io.File
 
+fun getFuel(mass: Int) = (mass / 3) - 2
+
+fun main() {
+    val masses = File("inputs/day1.txt").readLines().map { it.toInt() }
+
+    println("Total fuel: ${masses.sumBy(::getFuel)}")
+
+    val correctedFuel = masses.sumBy {
+        var totalFuel = 0
+        var neededFuel = getFuel(it)
+        while (neededFuel > 0) {
+            totalFuel += neededFuel
+            neededFuel = getFuel(neededFuel)
+        }
+        totalFuel
+    }
+
+    println("Corrected fuel: $correctedFuel")
 }
