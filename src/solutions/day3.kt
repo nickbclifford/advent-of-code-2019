71306ea923b1b60c0fc44c7aa700eeff93c9e836
Nick Clifford <nick@nickclifford.me>
2019-12-03 00:48:19 -0600

    day 3

---
+++ src/solutions/day3.kt
@@ -0,0 +1,58 @@
+package solutions
+
+import java.io.File
+import kotlin.math.abs
+
+val points = mutableListOf<Pair<Int, Int>>()
+
+fun processWire(wire: List<Pair<Char, Int>>): MutableList<Pair<Int, Int>> {
+    var x = 0
+    var y = 0
+
+    val holdPoints = mutableListOf<Pair<Int, Int>>()
+
+    for ((direction, distance) in wire) {
+        when (direction) {
+            'U' -> {
+                repeat(distance) {
+                    holdPoints.add(x to ++y)
+                }
+            }
+            'D' -> {
+                repeat(distance) {
+                    holdPoints.add(x to --y)
+                }
+            }
+            'L' -> {
+                repeat(distance) {
+                    holdPoints.add(--x to y)
+                }
+            }
+            'R' -> {
+                repeat(distance) {
+                    holdPoints.add(++x to y)
+                }
+            }
+        }
+    }
+
+    points.addAll(holdPoints.distinct())
+    return holdPoints
+}
+
+fun main() {
+    val wires = File("inputs/day3.txt").readLines().map { wire ->
+        val paths = wire.split(',')
+        paths.map { it[0] to it.substring(1).toInt() }
+    }
+
+    val wirePoints = wires.map(::processWire)
+
+    val intersections = points.groupingBy { it }.eachCount().filterValues { it == 2 }.keys
+    val distances = intersections.map { (x, y) -> abs(x) + abs(y) }
+
+    println("Manhattan distance: ${distances.min()}")
+
+    val steps = intersections.map { i -> wirePoints.sumBy { it.indexOf(i) + 1 } }
+    println("Fewest steps: ${steps.min()}")
+}
